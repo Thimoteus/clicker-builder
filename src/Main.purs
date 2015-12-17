b18e115bd4bf47c0e84ebd4a399165e0cb045fe1
thimoteus <thimoteus@teknik.io>
2015-12-17 13:48:26 -0800

    preliminaries of different screens

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,6 @@ import Prelude
 
 import Control.Monad.Eff (Eff())
 
-import Data.Maybe (Maybe(..))
 import Data.Maybe.Unsafe (fromJust)
 import Data.Nullable (toMaybe)
 import Data.List (List(..), (:))
@@ -27,6 +26,7 @@ type State a = { clicks :: Number
                , cps :: Number
                , clickBurst :: Number
                , upgradesBought :: List Upgrade
+               , screen :: Screen
                | a }
 type GameState = State ()
 type Environment = State ( channel :: Channel Action )
@@ -37,29 +37,39 @@ mkEnv channel state = { clicks: state.clicks
                       , channel: channel
                       , clickBurst: state.clickBurst
                       , upgradesBought: state.upgradesBought
+                      , screen: state.screen
                       , cps: state.cps }
 
 initialState :: GameState
 initialState = { clicks: 0.0
                , cps: 0.0
                , upgradesBought: Nil
+               , screen: Game
                , clickBurst: 1.0 }
 
 data Action = Click
             | AutoClick
             | Buy Upgrade
+            | Switch Screen
             | Reset
 
 data Upgrade = CPS Number
              | Burst Number
 
+data Screen = Game
+            | Settings
+            | SaveLoad
+
 instance showUpgrade :: Show Upgrade where
   show (CPS n) = "buy CPS upgrade +" ++ show n
   show (Burst n) = "buy click upgrade +" ++ show n
 
-genericButtonWithText :: Action -> String -> Environment -> ReactElement
+genericButtonWithText :: Action -> String -> Component
 genericButtonWithText act str env =
   D.button [P.onClick \ _ -> send env.channel act] [D.text str]
+
+foldComponent :: Array Component -> Component
+foldComponent arr = \ env -> D.div' $ arr <*> [env]
 
 clickButton :: Component
 clickButton = genericButtonWithText Click "click me!"
@@ -68,9 +78,7 @@ resetButton :: Component
 resetButton = genericButtonWithText Reset "reset"
 
 buyButton :: Component
-buyButton env = D.div' $ flip apply [env] [ upgrade (CPS 1.0)
-                                          , upgrade (Burst 1.0)
-                                          ]
+buyButton = foldComponent [upgrade (CPS 1.0), upgrade (Burst 1.0)]
 
 upgrade :: Upgrade -> Component
 upgrade up = genericButtonWithText (Buy up) (show up)
@@ -82,7 +90,28 @@ currentClicks :: Component
 currentClicks env = D.text (show env.clicks)
 
 view :: Component
-view env = D.div' $ flip apply [env] [controls, currentClicks]
+view env = case env.screen of
+                Game -> showGame env
+                Settings -> showSettings env
+                SaveLoad -> showSaveLoad env
+
+showGame :: Component
+showGame = foldComponent [changeView, controls, currentClicks]
+
+showSettings :: Component
+showSettings = foldComponent [changeView, settingsText]
+  where
+    settingsText _ = D.text "Settings"
+
+showSaveLoad :: Component
+showSaveLoad = foldComponent [changeView, saveLoadText]
+  where
+    saveLoadText _ = D.text "Save/Load game"
+
+changeView :: Component
+changeView env = D.div' [ genericButtonWithText (Switch Game) "Game" env
+                        , genericButtonWithText (Switch Settings) "Settings" env
+                        , genericButtonWithText (Switch SaveLoad) "Save/Load" env ]
 
 component :: Environment -> ReactClass Unit
 component env = createClass (spec unit \ _ -> pure (view env))
@@ -90,8 +119,13 @@ component env = createClass (spec unit \ _ -> pure (view env))
 step :: Action -> GameState -> GameState
 step Click state = state { clicks = state.clicks + state.clickBurst }
 step AutoClick state = state { clicks = state.clicks + state.cps }
-step (Buy (CPS n)) state = state { cps = state.cps + n, upgradesBought = (CPS n) : state.upgradesBought }
-step (Buy (Burst n)) state = state { clickBurst = state.clickBurst + n, upgradesBought = (Burst n) : state.upgradesBought }
+step (Buy (CPS n)) state =
+  state { cps = state.cps + n
+        , upgradesBought = (CPS n) : state.upgradesBought }
+step (Buy (Burst n)) state =
+  state { clickBurst = state.clickBurst + n
+        , upgradesBought = (Burst n) : state.upgradesBought }
+step (Switch screen) state = state { screen = screen }
 step Reset _ = initialState
 
 updateRate :: Signal Number
